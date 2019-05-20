f7c57673f5a70b9d37c83b274ccf94d03af12b17
Pedro Gomes <pedrogomes94@gmail.com>
2019-05-20 15:28:48 +0200

    add Notification component and redux

---
+++ src/reducers/_notification.ts
@@ -0,0 +1,37 @@
+// -- Constants ------------------------------------------------------------- //
+const NOTIFICATION_SHOW = "notification/NOTIFICATION_SHOW";
+const NOTIFICATION_HIDE = "notification/NOTIFICATION_HIDE";
+
+// -- Actions --------------------------------------------------------------- //
+let timeoutHide: any;
+
+export const notificationShow = (message: string, error = false) => (
+  dispatch: any
+) => {
+  clearTimeout(timeoutHide);
+  dispatch({ type: NOTIFICATION_SHOW, payload: { message, error } });
+  timeoutHide = setTimeout(() => dispatch({ type: NOTIFICATION_HIDE }), 15000);
+};
+
+// -- Reducer --------------------------------------------------------------- //
+const INITIAL_STATE = {
+  show: false,
+  error: false,
+  message: ""
+};
+
+export default (state = INITIAL_STATE, action: any) => {
+  switch (action.type) {
+    case NOTIFICATION_SHOW:
+      return {
+        ...state,
+        show: true,
+        message: action.payload.message,
+        error: action.payload.error
+      };
+    case NOTIFICATION_HIDE:
+      return { ...state, show: false, message: "", error: false };
+    default:
+      return state;
+  }
+};

9b330be2d530b12d0e2960e4f07200ace2cf2cf1
Pedro Gomes <pedrogomes94@gmail.com>
2019-05-21 22:03:54 +0200

    refactor ListItem

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import { colors, fonts, shadows, transitions } from "../styles";
+import { colors, fonts, transitions } from "../styles";
 import { IMenuItem, IOrderItem } from "../helpers/types";
 import Button from "../components/Button";
 import Loader from "../components/Loader";
@@ -13,6 +13,7 @@ import {
 } from "../redux/_order";
 import { toFixed } from "../helpers/bignumber";
 import QRCodeDisplay from "../components/QRCodeDisplay";
+import ListItem from "../components/ListItem";
 import arrow from "../assets/arrow.png";
 import success from "../assets/success.png";
 import error from "../assets/error.png";
@@ -116,93 +117,6 @@ const STitle = styled.h4`
   margin: 0;
 `;
 
-interface IListItemStyleProps {
-  onClick?: any;
-}
-
-const SListItem = styled.div<IListItemStyleProps>`
-  transition: ${transitions.base};
-
-  border-radius: 6px;
-  border: 1px solid rgb(${colors.lightGrey});
-  margin-bottom: 16px;
-
-  transform: translate3d(0, 0, 0);
-  box-shadow: ${shadows.base};
-
-  ${({ onClick }) =>
-    onClick &&
-    `
-    cursor: pointer;
-    @media (hover: hover) {
-      &:hover {
-        transform: translate3d(0, -1px, 0);
-        box-shadow: ${shadows.hover};
-      }
-    }
-  `}
-`;
-
-const SListItemContainer = styled.div`
-  width: 100%;
-  padding: 16px 24px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const SListItemActions = styled.div`
-  width: 100%;
-  display: flex;
-  border-top: 1px solid rgb(${colors.lightGrey});
-`;
-
-const SListItemButton = styled.div`
-  transition: ${transitions.base};
-  width: 100%;
-  padding: 12px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: rgb(${colors.grey});
-  font-weight: ${fonts.weight.semibold};
-  border-right: 1px solid rgb(${colors.lightGrey});
-  cursor: pointer;
-  &:last-child {
-    border-right: none;
-  }
-  @media (hover: hover) {
-    &:hover span {
-      transform: translate3d(0, -1px, 0);
-    }
-  }
-`;
-
-interface IListItemDetailsStyleProps {
-  alignRight?: boolean;
-}
-
-const SListItemDetails = styled.div<IListItemDetailsStyleProps>`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: ${({ alignRight }) =>
-    alignRight ? "flex-end" : "flex-start"};
-`;
-
-const SListItemImage = styled.div`
-  width: 120px;
-  height: 120px;
-  margin-right: 20px;
-  & img {
-    width: 100%;
-  }
-`;
-
-const SListItemText = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 const SListItemName = styled.div`
   font-size: ${fonts.size.large};
 `;
@@ -210,21 +124,6 @@ const SListItemName = styled.div`
 const SListItemDescription = styled.div`
   color: rgb(${colors.grey});
   margin-top: 8px;
-`;
-
-const SListItemPrice = styled.div`
-  width: 100%;
-  text-align: right;
-  font-size: ${fonts.size.large};
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-const SListItemQuantity = styled(SListItemPrice)``;
-
-const SListItemSubtotal = styled(SListItemDescription)`
-  text-align: right;
 `;
 
 interface IModalStyleProps {
@@ -343,28 +242,11 @@ class Order extends React.Component<any, any> {
             </SColumnHeader>
             <SColumnList>
               {menu.map((item: IMenuItem) => (
-                <SListItem
+                <ListItem
                   key={item.name}
+                  item={item}
                   onClick={() => this.props.orderAddItem(item)}
-                >
-                  <SListItemContainer>
-                    <SListItemDetails>
-                      <SListItemImage>
-                        <img src={item.image} alt={item.name} />
-                      </SListItemImage>
-                      <SListItemText>
-                        <SListItemName>{item.name}</SListItemName>
-                        <SListItemDescription>
-                          {item.description}
-                        </SListItemDescription>
-                      </SListItemText>
-                    </SListItemDetails>
-                    <SListItemPrice>{`$ ${toFixed(
-                      item.price,
-                      2
-                    )}`}</SListItemPrice>
-                  </SListItemContainer>
-                </SListItem>
+                />
               ))}
             </SColumnList>
           </SColumn>
@@ -374,41 +256,15 @@ class Order extends React.Component<any, any> {
             </SColumnHeader>
             <SColumnList>
               {items.map((item: IOrderItem) => (
-                <SListItem key={item.name}>
-                  <SListItemContainer>
-                    <SListItemDetails>
-                      <SListItemText>
-                        <SListItemName>{item.name}</SListItemName>
-                        <SListItemDescription>
-                          {item.description}
-                        </SListItemDescription>
-                      </SListItemText>
-                    </SListItemDetails>
-                    <SListItemDetails alignRight>
-                      <SListItemText>
-                        <SListItemQuantity>{`x ${
-                          item.quantity
-                        }`}</SListItemQuantity>
-                        <SListItemSubtotal>{`$ ${toFixed(
-                          item.price * item.quantity,
-                          2
-                        )}`}</SListItemSubtotal>
-                      </SListItemText>
-                    </SListItemDetails>
-                  </SListItemContainer>
-                  <SListItemActions>
-                    <SListItemButton
-                      onClick={() => this.props.orderRemoveItem(item)}
-                    >
-                      <span>{"Remove"}</span>
-                    </SListItemButton>
-                    <SListItemButton
-                      onClick={() => this.props.orderAddItem(item)}
-                    >
-                      <span>{"Add"}</span>
-                    </SListItemButton>
-                  </SListItemActions>
-                </SListItem>
+                <ListItem
+                  noImage
+                  key={item.name}
+                  item={item}
+                  actions={[
+                    { label: "Remove", callback: this.props.orderRemoveItem },
+                    { label: "Add", callback: this.props.orderAddItem }
+                  ]}
+                />
               ))}
             </SColumnList>
             <SColumnFooter>

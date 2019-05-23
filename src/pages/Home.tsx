5fff79598a3c8fd1dc24f08e873793ec5f13fc46
Pedro Gomes <pedrogomes94@gmail.com>
2019-05-23 19:13:42 +0200

    uncomment web3connect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
-// import Web3Connect from "web3connect";
+import Web3Connect from "web3connect";
 import { Link } from "react-router-dom";
 import PageWrapper from "../components/PageWrapper";
 import Card from "../components/Card";
@@ -20,7 +20,7 @@ class Home extends React.Component<any, any> {
       <PageWrapper row center>
         <Card shadow margin={20}>
           <h4>{`WalletConnect Pay`}</h4>
-          {/* <Web3Connect onConnect={this.onConnect} onClose={this.onClose} /> */}
+          <Web3Connect onConnect={this.onConnect} onClose={this.onClose} />
         </Card>
         <Card shadow margin={20}>
           <h4>{`Order Menu`}</h4>

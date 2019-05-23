cf301c47f4456941b3d3c100d7209cda502ede2d
Pedro Gomes <pedrogomes94@gmail.com>
2019-05-23 15:14:03 +0200

    update _admin reducer

--- src/redux/_admin.ts
+++ src/redux/_admin.ts
@@ -1,47 +1,116 @@
-// import { apiAuthAdmin } from "../helpers/api";
+import { notificationShow } from "./_notification";
 
 // -- Constants ------------------------------------------------------------- //
-const ADMIN_AUTHENTICATE_REQUEST = "admin/ADMIN_AUTHENTICATE_REQUEST";
-const ADMIN_AUTHENTICATE_SUCCESS = "admin/ADMIN_AUTHENTICATE_SUCCESS";
-const ADMIN_AUTHENTICATE_FAILURE = "admin/ADMIN_AUTHENTICATE_FAILURE";
+const ADMIN_CONNECT_REQUEST = "admin/ADMIN_CONNECT_REQUEST";
+const ADMIN_CONNECT_SUCCESS = "admin/ADMIN_CONNECT_SUCCESS";
+const ADMIN_CONNECT_FAILURE = "admin/ADMIN_CONNECT_FAILURE";
+
+const ADMIN_UPDATE_SIGNUP_FORM = "admin/ADMIN_UPDATE_SIGNUP_FORM";
+
+const ADMIN_SUBMIT_SIGNUP_REQUEST = "admin/ADMIN_SUBMIT_SIGNUP_REQUEST";
+const ADMIN_SUBMIT_SIGNUP_SUCCESS = "admin/ADMIN_SUBMIT_SIGNUP_SUCCESS";
+const ADMIN_SUBMIT_SIGNUP_FAILURE = "admin/ADMIN_SUBMIT_SIGNUP_FAILURE";
 
 const ADMIN_CLEAR_STATE = "admin/ADMIN_CLEAR_STATE";
 
 // -- Actions --------------------------------------------------------------- //
 
-async function apiAuthAdmin(credentials: any) {
-  return true;
-}
-
-export const adminAuthenticate = (credentials: any) => async (
-  dispatch: any
-) => {
-  dispatch({ type: ADMIN_AUTHENTICATE_REQUEST });
+export const adminConnectWallet = (provider: any) => async (dispatch: any) => {
+  dispatch({ type: ADMIN_CONNECT_REQUEST });
   try {
-    const result = await apiAuthAdmin(credentials);
-    console.log(result); // tslint:disable-line
-    dispatch({ type: ADMIN_AUTHENTICATE_SUCCESS });
+    // TODO: Replace these
+    const web3 = null;
+    const address = "0x";
+    const chainId = 1;
+    const businessName = "Coffesshop";
+
+    if (businessName) {
+      dispatch({
+        type: ADMIN_CONNECT_SUCCESS,
+        payload: {
+          web3,
+          address,
+          chainId,
+          businessName
+        }
+      });
+      // window.browserHistory.push("/admin");
+    } else {
+      dispatch({ type: ADMIN_CONNECT_FAILURE });
+      // window.browserHistory.push("/signup");
+    }
   } catch (error) {
     console.error(error); // tslint:disable-line
-    dispatch({ type: ADMIN_AUTHENTICATE_FAILURE });
+    dispatch(notificationShow(error.message, true));
+    dispatch({ type: ADMIN_CONNECT_FAILURE });
   }
+};
+
+export const adminSubmitSignUp = () => async (dispatch: any, getState: any) => {
+  dispatch({ type: ADMIN_SUBMIT_SIGNUP_REQUEST });
+  try {
+    // const { signUpForm } = getState().admin;
+    // const profile = { name: signUpForm.name, type: signUpForm.type };
+    // await setSpacePrivate("profile", profile);
+
+    // await apiSendEmailVerification(signUpForm.email)
+
+    dispatch({ type: ADMIN_SUBMIT_SIGNUP_SUCCESS });
+
+    // window.browserHistory.push("/admin");
+  } catch (error) {
+    console.error(error); // tslint:disable-line
+    dispatch(notificationShow(error.message, true));
+    dispatch({ type: ADMIN_SUBMIT_SIGNUP_FAILURE });
+  }
+};
+
+export const adminUpdateSignUpForm = (updatedSignUpForm: any) => async (
+  dispatch: any,
+  getState: any
+) => {
+  let { signUpForm } = getState().admin;
+  signUpForm = {
+    ...signUpForm,
+    ...updatedSignUpForm
+  };
+  dispatch({ type: ADMIN_UPDATE_SIGNUP_FORM, payload: signUpForm });
 };
 
 export const adminClearState = () => ({ type: ADMIN_CLEAR_STATE });
 
 // -- Reducer --------------------------------------------------------------- //
 const INITIAL_STATE = {
-  loading: false
+  loading: false,
+  address: "",
+  web3: null,
+  chainId: 1,
+  businessName: "",
+  signUpForm: {
+    name: "",
+    type: "cafe",
+    country: "DE",
+    email: ""
+  }
 };
 
 export default (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
-    case ADMIN_AUTHENTICATE_REQUEST:
+    case ADMIN_CONNECT_REQUEST:
       return { ...state, loading: true };
-    case ADMIN_AUTHENTICATE_SUCCESS:
+    case ADMIN_CONNECT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        web3: action.payload.web3,
+        address: action.payload.address,
+        chainId: action.payload.chainId,
+        businessName: action.payload.businessName
+      };
+    case ADMIN_CONNECT_FAILURE:
       return { ...state, loading: false };
-    case ADMIN_AUTHENTICATE_FAILURE:
-      return { ...state, loading: false };
+    case ADMIN_UPDATE_SIGNUP_FORM:
+      return { ...state, signUpForm: action.payload };
     case ADMIN_CLEAR_STATE:
       return { ...state, ...INITIAL_STATE };
     default:
